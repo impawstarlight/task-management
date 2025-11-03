eff1e8017f85ec36b8f891ffdd46f25088dc7478
Abdul Kaium <abdulkaium1024@gmail.com>
2025-11-04 01:36:29 +0600

    Added 5 tasks and saved tasks response as a JSON file

--- src/index.js
+++ src/index.js
@@ -6,15 +6,48 @@ app.get("/", (req, res) => {
   res.send("Task Management API is running!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
 const tasks = [
-  { id: 1, title: "Wake up", completed: true },
-  { id: 2, title: "Go to bed", completed: false },
+  {
+    id: 1,
+    title: "Wake up",
+    completed: true,
+    priority: "high",
+    createdAt: new Date("November 1, 2025 00:00:00"),
+  },
+  {
+    id: 2,
+    title: "Have breakfast",
+    completed: false,
+    priority: "medium",
+    createdAt: new Date("2025-11-01T08:00:00"),
+  },
+  {
+    id: 3,
+    title: "Study cybersecurity",
+    completed: false,
+    priority: "high",
+    createdAt: new Date("2025-11-01T08:00:00Z"),
+  },
+  {
+    id: 4,
+    title: "Take a break",
+    completed: false,
+    priority: "low",
+    createdAt: new Date(),
+  },
+  {
+    id: 5,
+    title: "Go to bed",
+    completed: false,
+    priority: "medium",
+    createdAt: new Date(),
+  },
 ];
 
 app.get("/tasks", (req, res) => {
   res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
